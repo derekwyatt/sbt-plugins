f8a83e4122f7d0a5af0e48f005b5070eb90261ad
Derek Wyatt <dwyatt@rim.com>
2012-03-14 10:36:37 -0400

    Added an example that provides an extended custom compile.  We create a 'new' compile task that depends on 'compile' and ensures that 'compile' happens first.

--- compile-config/README.md
+++ compile-config/README.md
@@ -1,6 +1,18 @@
 # "New Compiler Config" SBT Plugin #
 
-The idea here is to create a new "thing" that extends the compile.
+The idea here is to create a new "thing" that extends the compile.  The goal of
+this plugin is to create a new compile operation that allow us to do the
+following:
+
+    > new:compile
+
+or
+
+    > ~new:compile
+
+When we do either of those, we want the 'standard' compile to run first and then
+our new compile.  If we do `compile:compile` (or just `compile`) then our 'new'
+compile shouldn't run.
 
 ## plugin/ ##
 
--- compile-config/plugin/src/main/scala/org/derekwyatt/CompileConfigPlugin.scala
+++ compile-config/plugin/src/main/scala/org/derekwyatt/CompileConfigPlugin.scala
@@ -4,24 +4,53 @@ import sbt._
 import sbt.Keys._
 
 object CompileConfigPlugin extends Plugin {
-  import Keys._
-  val watchedFiles = TaskKey[Seq[File]]("watched-files", "The files to watch in dir-to-watch.")
-  val compile = TaskKey[Unit]("compile", "Task that 'compiles' the files that have changed.")
+  import Keys._ // pulls in dirToWatch
+
+  // Internal Task to hold the dynamic set of sources
+  val newSources = TaskKey[Seq[File]]("new-sources")
+
+  // The definition of our "new" configuration.  We're specializing "compile" here
   val NewConfig = config("new") extend(Compile)
-  //def compileConfigSettingHelper = Seq(
+
+  // Here we pull in all of the configurations from the Defaults and then specialize some
+  // stuff for our new configuration
   lazy val compileConfigSettings: Seq[Setting[_]] = inConfig(NewConfig)(Defaults.configSettings ++ Seq(
+    // BE CAREFUL HERE.  sourceDirectory evaluates to 'src/new' because we're inside the
+    // "new" configuration.  If you want to have dirToWatch be 'src/watching' instead of
+    // 'src/new/watching' then you need to use '(sourceDirectory in Compile) / "watching"'
+    // instead
     dirToWatch <<= sourceDirectory / "watching",
-    watchedFiles <<= dirToWatch map { d => { IO.listFiles(d).toSeq } },
-    watchSources <++= watchedFiles,
-    (compile in Compile) ~= { _ => compileTask }
-  ))
-  def compileTask: Unit = (watchedFiles, target) map { (files, dir) =>
-    files foreach { file =>
-      val targetFile = dir / file.getName
-      if (!targetFile.exists || targetFile.lastModified < file.lastModified) {
-        println("===> compiled %s".format(file.getName))
-        IO.touch(targetFile)
+
+    // The task that will populate our newSources simply looks up all the files in the
+    // dirToWatch directory.  You can always prune this with a FileFilter if you'd like.
+    newSources <<= dirToWatch map { d => { IO.listFiles(d).toSeq } },
+
+    // Our compile step.  We'll delegate to the 'newCompile' method, but we're going
+    // to also depend on the compile in Compile.  This allows us to say
+    // new:compile as well as compile.  If we say new:compile then the compile task will
+    // run first and must succeed first.
+    (compile in NewConfig) <<= newCompile dependsOn (compile in Compile)
+
+    // This last bit is a bit tricky.  We have to add our newSources that are defined in
+    // NewConfig to the 'watchSources' that are defined in the global space.  The current
+    // block we're in is the 'inConfig(NewConfig)...' section so we need to close that off
+    // first.  If we append to 'watchSources' inside our NewConfig then any changes to them
+    // won't trigger anything at all.
+  )) ++ Seq(watchSources <++= (newSources in NewConfig))
+
+  // Our compile function.  This is pretty dumb and simplistic but the compile logic isn't
+  // the goal of this example.  If you need to then you can use the caching mechanism that
+  // SBT provides or whatever else.  This is just a toy compile.
+  def newCompile = (newSources, target) map { (fs, dir) =>
+      fs foreach { file =>
+        val targetFile = dir / file.getName
+        if (!targetFile.exists || targetFile.lastModified < file.lastModified) {
+          println("===> compiled %s".format(file.getName))
+          IO.touch(targetFile)
+        }
       }
+      // Compilers respond with Analysis objects, and we don't really have any to
+      // provide, so we're just using an Empty one.
+      sbt.inc.Analysis.Empty
     }
-  }
 }
